c0f1a5180fb32ba0976022222402626833b3ba16
¨lautaroagustin¨ <lautaron16@hotmail.com>
2022-06-20 19:03:18 -0300

    ¨complementario¨

---
+++ script.js
@@ -0,0 +1,8 @@
+let entradas = parseInt(prompt("Ingresar la cantidad de entradas que quieras comprar."));
+
+for(i= 1 ; i <= entradas; i++){
+    let nombres= prompt("Ingresar nombre del espectador.")
+    alert("Hola, " + nombres + ". Sos el espectador número " + i + ".")
+}
+
+
